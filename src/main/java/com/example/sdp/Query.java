193b6975286ec61947c3adbe0d34289f3f67b5f4
Bartłomiej Mazurek <mazurekb02@gmail.com>
2020-12-16 13:53:04 +0100

    into the ex2 we go

--- src/main/java/com/example/sdp/Query.java
+++ src/main/java/com/example/sdp/Query.java
@@ -1,6 +1,9 @@
 package com.example.sdp;
 
 import java.sql.*;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Random;
 
 public class Query {
 
@@ -15,6 +18,35 @@ public class Query {
         Statement s = c.createStatement();
         return s.executeQuery(selectQuery);
     }
+
+    public static void insert(int n) throws SQLException{
+        String[] names = new String[] { "Liam", "Noah", "Oliver", "William", "Elijah", "James", "Benjamin", "Lucas",
+                "Mason", "Ethan", "Alexander", "Henry", "Jacob", "Michael", "Daniel", "Logan", "Jackson",
+                "Sebastian","Jack", "Aiden", "Owen", "Samuel", "Matthew", "Joseph", "Levi",
+                "Mateo", "David", "John", "Wyatt", "Carter", "Julian", "Luke", "Grayson", "Isaac",
+                "Jayden", "Theodore", "Gabriel", "Anthony", "Dylan", "Leo", "Lincoln"};
+        Random rand = new Random();
+        StringBuilder sb = new StringBuilder("INSERT INTO PUBLIC.NAMES(\"id_name\", \"name\") VALUES ");
+
+        for(int i=0; i<n; i++){
+            sb.append("(" + (i+1) + ", '" + names[rand.nextInt(names.length)] + "')");
+            if(i != n-1){
+                sb.append(", ");
+            }
+        }
+
+        String query = sb.toString();
+        Instant start = Instant.now();
+
+        Connection c = establishConnection();
+        Statement s = c.createStatement();
+        s.execute(query);
+
+        Instant end = Instant.now();
+        System.out.println("Execution time for inserts in number of " + n + ": " +
+                (Duration.between(start, end)).toMillis());
+        }
+
 
     private static Connection establishConnection() {
         String URL = "jdbc:postgresql://localhost/sdp";
