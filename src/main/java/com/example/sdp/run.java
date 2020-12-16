cbceac4efcbfed475ddee71c10d61d71cc200de3
Bartłomiej Mazurek <mazurekb02@gmail.com>
2020-12-16 21:06:01 +0100

    results file added

--- src/main/java/com/example/sdp/run.java
+++ src/main/java/com/example/sdp/run.java
@@ -11,19 +11,40 @@ public class run {
 //        Query.query("INSERT INTO PUBLIC.ITEMS(\"id_item\", \"name_item\") values (4, 'Zatruty miecz')")
         Query.query("DELETE FROM PUBLIC.NAMES");
 
-        Query.insert(10000);
+        Query.insert(1000);
+        showDatabase();
+
+        Query.query("UPDATE PUBLIC.NAMES SET \"name\" = '@@@@@@@@@@@@' WHERE \"name\" = 'Oliver'");
+
+
+        System.out.println("--------------------------------------------------------");
+        Query.query("DELETE FROM PUBLIC.NAMES");
+
+        Query.insert(100000);
+        showDatabase();
+
+        Query.query("UPDATE PUBLIC.NAMES SET \"name\" = '@@@@@@@@@@@@' WHERE \"name\" = 'Oliver'");
+
+        System.out.println("--------------------------------------------------------");
+        Query.query("DELETE FROM PUBLIC.NAMES");
+
+        Query.insert(1000000);
+        showDatabase();
+
+        Query.query("UPDATE PUBLIC.NAMES SET \"name\" = '@@@@@@@@@@@@' WHERE \"name\" = 'Oliver'");
+    }
+
+    public static void showDatabase() throws SQLException {
         Instant start = Instant.now();
         ResultSet rs = Query.select("SELECT * FROM PUBLIC.NAMES");
         while(rs.next()) {
             int id = rs.getInt("id_name");
             String name = rs.getString("name");
-            System.out.println(id + ". " + name);
+//            System.out.println(id + ". " + name);
         }
         Instant end = Instant.now();
         System.out.println("Execution time for SELECT: " +
                 (Duration.between(start, end)).toMillis() + "ms");
-
-
     }
 
 }
